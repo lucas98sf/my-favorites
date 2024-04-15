06c744edf43fde36f82a8f91989ef7b06f55da9f
Lucas Ferreira <lucas.98sf@gmail.com>
2024-04-15 07:08:17 -0300

    add eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
-export default async function PrivatePage() {
-  return <p></p>
+import Profile from '@/components/Profile'
+import { createClient } from '@/lib/supabase/server'
+
+export default async function ProfilePage() {
+  const supabase = createClient()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  return <Profile user={user} />
 }
